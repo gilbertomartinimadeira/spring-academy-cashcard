326c7eb64078ee42ce2cc967642ab5ed11fe7998
Gilberto Martini <gilberto.martini@e-hps.com>
2025-06-26 19:44:28 -0300

    Added a first test

---
+++ src/test/java/crossmade/cashcard/CashCardJsonTest.java
@@ -0,0 +1,12 @@
+package crossmade.cashcard;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class CashCardJsonTest {
+
+    @Test
+    public void myFirstTest() {
+        assertThat(42).isEqualTo(42);
+    }
+}
