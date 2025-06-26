d034c21be53408d64c45b24d764b523c75308e92
Gilberto Martini <gilberto.martini@e-hps.com>
2025-06-26 20:24:45 -0300

    Adds json test

---
+++ src/test/java/crossmade/cashcard/CashCard.java
@@ -0,0 +1,5 @@
+package crossmade.cashcard;
+
+public record CashCard(Long id, Double amount) {
+
+}
--- src/test/java/crossmade/cashcard/CashCardJsonTest.java
+++ src/test/java/crossmade/cashcard/CashCardJsonTest.java
@@ -1,12 +1,33 @@
 package crossmade.cashcard;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
 import static org.assertj.core.api.Assertions.*;
 
+import java.io.IOException;
+
 public class CashCardJsonTest {
+
+    @Autowired
+    private JacksonTester<CashCard> json;
+
+    @Test
+    void cashCardSerializationTest() throws IOException {
+        CashCard cashCard = new CashCard(99L, 123.45);
+        assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
+        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id")
+                .isEqualTo(99);
+        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount")
+                .isEqualTo(123.45);
+    }
 
     @Test
     public void myFirstTest() {
         assertThat(42).isEqualTo(42);
     }
+
 }
